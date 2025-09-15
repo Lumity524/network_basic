e79b03cc8fd7e71f4b788c6e63cfbd9935866822
노현승 <seunglee0807@gmail.com>
2025-09-15 19:29:06 +0900

    test

--- ch02/static/index.js
+++ ch02/static/index.js
@@ -1,1 +1,14 @@
+let count = 0;
+const numberDiv = document.getElementById('num');
+const increaseBtn = document.getElementById('inc');
+const sendBtn = document.getElementById('send');
+
+increaseBtn.addEventListener('click', () => {
+    count++;
+    numberDiv.textContent = count;
+});
+
+sendBtn.addEventListener('click', () => {
+    alert('현재 숫자: ' + count + ' 전송!');
+});
 
